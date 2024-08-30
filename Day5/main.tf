4bf0273f22e7d3755dffd38973757a879c32e1b1
ravindersinghsond <103376064+ravindersinghsond@users.noreply.github.com>
2024-08-30 08:01:53 +0000

    main.tf updated

--- Day5/main.tf
+++ Day5/main.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "vpc_dev" {
 
 resource "aws_subnet" "vpc_dev_public" {
   vpc_id = aws_vpc.vpc_dev.id
-  cidr_block = "10.1.0.0/24"
+  cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
   tags = {
