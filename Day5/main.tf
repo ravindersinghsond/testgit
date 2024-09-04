2b5af24a99c1de436afabf05b6751f4664cf3db5
ravindersinghsond <103376064+ravindersinghsond@users.noreply.github.com>
2024-09-04 13:37:29 +0000

    Day6 and remote file repo.txt added

--- Day5/main.tf
+++ Day5/main.tf
@@ -6,3 +6,8 @@ resource "aws_subnet" "vpcf_dev_public_subnet" {
   vpc_id = aws_vpc.vpc_dev.id
   cidr_block = "10.0.1.0/24"
 }
+
+resource "aws_subnet" "vpcf_dev_private_subnet" {
+  vpc_id = aws_vpc.vpc_dev.id
+  cidr_block = "10.0.2.0/24"
+}
---
+++ remote1.tf
@@ -0,0 +1,1 @@
+this file is created when the remote user was modifying main.tf locally
